db2d684c5e12b532955262c7395cb8fa23e36774
ClashMaster13 <ClashMaster13@users.noreply.github.com>
2026-06-25 19:32:56 +0530

    Re-enable VitePWA to generate sw.js and un-strand cached PWA clients

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'script',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg', 'favicon.svg'],
+      manifest: false
+    })
   ],
 })
