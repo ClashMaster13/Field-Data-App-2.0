dafa8543300eb778b11afe9e14b9469443b49389
ClashMaster13 <ClashMaster13@users.noreply.github.com>
2026-06-25 19:09:49 +0530

    Disable VitePWA to fix fatal Vercel Rollup bug

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'script',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg', 'favicon.svg'],
-      manifest: false
-    })
+    react()
   ],
 })
